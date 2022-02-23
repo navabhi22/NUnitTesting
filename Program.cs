00c3e3e6211e431658699befea57c1663156336c
navabhi22 <navabhi22@gmail.com>
2022-02-23 23:09:00 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace NUnitTesting
         static void Main(string[] args)
         {
             Console.WriteLine("Programs of NUnit Testing");
+
+            Console.WriteLine("enter the return amount");
+            int Amt = Convert.ToInt32(Console.ReadLine());
+            int MinNotes = FewerNotes.MinimumNotes(Amt);
+            Console.WriteLine("The min no. of Notes required = " + MinNotes);
+
         }
     }
 }
